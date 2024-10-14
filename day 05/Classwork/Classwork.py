03b28d2f558d486f2517f1ab6323f45df75af2f3
Kyllian-Mbappe2025 <sandrokvinchia2012@gmail.com>
2024-10-14 13:26:51 +0400

    commit

---
+++ day 05/Classwork/Classwork.py
@@ -0,0 +1,5 @@
+print(age = 26)
+country = "Georgia"
+game = "FC25"
+print(year = 2024)
+day = "4 octomber"
---
+++ day 06/homework/lesson.py
@@ -0,0 +1,68 @@
+# month - october
+# age /d 17
+# chocolate:bueno
+# nomber _ 27
+# subject @ goa academy
+# day =sunday
+# game =call of duty vanguard
+# name ; sandro
+# username < kvinchia
+# team : locomitive
+# iq + 150
+# shop - zoomer
+# time * 7 a.m
+# player p> yamal
+# lider - irakli
+# country . georgia
+# capital _ tbilisi
+# champions_leauge- 5
+# liga & la liga
+# club p> barcelona
+
+
+# month = "october"
+# age = 17
+# chocolate = "bueno"
+# nomber = 27
+# subject = "goa academy"
+# day = "sunday"
+# game = "call of duty vanguard"
+# name = "sandro"
+# username = "kvinchia"
+# team = "locomotive"
+# iq = "150"
+# shop = "zoomer"
+# time = "7 a.m"
+# player = "yamal"
+# lider = "irakli"
+# country = "georgia"
+# capital = "tbilisi"
+# champions_leauge = 5
+# liga = "la liga"
+# club = "barcelona"
+
+
+# name1 = "goa"
+# name2 = "best"
+# print(name1, name2)
+
+aura = 30
+print(aura + 10)
+
+name = "anna"
+score = 96
+
+print(name, score)
+
+# Storing the flight number
+flight_n = "BA0117"
+# ცვლადი,რომელიც ინახავს სტრინგს
+# Storing the flight information
+destination = "New York"
+# ცვლადი,რომელიც ინახავს სტრინგს
+distance = 1580
+# ცვლადი,რომელიც ინახავს ინტეჯერს
+
+print(flight_n)
+print(destination)
+print(distance)
