5385b93a17d8868f3803b24886404350fb7b8c7b
Kyllian-Mbappe2025 <sandrokvinchia2012@gmail.com>
2024-11-19 22:52:42 +0400

    commit

---
+++ level 11/classwork/lesson.py
@@ -0,0 +1,9 @@
+bool1 = 10 > 5 # True 
+bool2 = 3 != 3 # False
+
+# შედარების ოპერატორები: < > <= >= == !=
+
+# ლოგიკური ოპერატორები: and or
+
+print(bool1 and bool2) # False
+print(bool1 or bool2) # True
