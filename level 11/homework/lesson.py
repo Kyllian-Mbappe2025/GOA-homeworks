79c6c8e800f52aebc4a3120cd7b7ffb4ac077db2
Kyllian-Mbappe2025 <sandrokvinchia2012@gmail.com>
2024-11-23 00:41:47 +0400

    commit

--- level 11/homework/lesson.py
+++ level 11/homework/lesson.py
@@ -1,4 +1,22 @@
 bool1 = 9 > 5 # True
-bool2 = 7 = 8 # False
+bool2 = 7 > 8 # False
 bool3 = 10 == 10 # True
 bool4 = 4 != 4 # False 
+print(bool1,bool2,bool3,bool4)
+
+print(5 > 10 and False)
+print(7 != 9 or 5 < 2)
+print(6 >= 5 or 7 > 9)
+print(8 == 8)
+print( False or True)
+print(2 <= 1 and 10 > 18)
+print(True or False)
+print(29 != 29 and 7 > 20)
+print(1 < 5 or 9 < 41)
+print(6 != 8 and 3 > 5)
+
+number = int(input("Enter a number: "))
+print(number > 10 and number < 20)
+
+number2 = int(input("Enter your number "))
+print(number > 20 and number < 120)
