bd26f725dcac4db63bbcedd47bea51e8c4d96adc
Kyllian-Mbappe2025 <sandrokvinchia2012@gmail.com>
2024-12-07 00:33:21 +0400

    commit

---
+++ level 13/homework/lesson.py
@@ -0,0 +1,26 @@
+print(10 > 5)
+print(3 > 1)
+print(5 > 3)
+print(12 > 7)
+print(24 > 8)
+print(True or False)
+print(True or False)
+print(True or False)
+print(True or False)
+print(True or False)
+print(True or False)
+
+print(80 < 17)
+print(40 > 59)
+print(6 > 7)
+print(78 > 80)
+print(45 > 100)
+print(True and False)
+print(True and False)
+print(True and False)
+print(True and False)
+print(True and False)
+
+# squence -> თანმიმდევრობა
+# selection -> გამეორება
+# iteration -> არჩევა
