4a782319d03354da77f60417a367ec59132d3a86
Kyllian-Mbappe2025 <sandrokvinchia2012@gmail.com>
2024-12-20 15:23:40 +0400

    commit

---
+++ level 15/homework/lesson.py
@@ -0,0 +1,10 @@
+# algorithm --> ნაბიჯ ნაბიჯ ინსტრუქცია დავალების შესასრულებლად
+
+for i in range(100):
+    print(i)
+
+for i in range(151):
+    print(i)
+
+for i in range(1, 101):
+    print(f"GOA {i}")
