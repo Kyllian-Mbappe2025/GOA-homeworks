40718d4b02f2c3f12b93605c8c630e4385e188fe
Kyllian-Mbappe2025 <sandrokvinchia2012@gmail.com>
2025-01-24 19:15:42 +0400

    commit

---
+++ level 20/homework/lesson.py
@@ -0,0 +1,22 @@
+number = int(input("Enter the nuumber"))
+if number % 2 == 0:
+       print("it is even")
+else:
+       print("it is not even")
+
+
+age = int(input("Enter your age"))
+if age < 18:
+    print("You are not adult")
+else:
+    print("You are adult")
+
+
+if score >= 90:
+    print("A")
+elif score >= 80:
+    print("B")
+elif score >= 70:
+    print("C")
+else:
+    print("score is low")              
