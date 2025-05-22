6d8be4d64e3747465ab4e5be570fac11228bbe91
Kyllian-Mbappe2025 <sandrokvinchia2012@gmail.com>
2025-05-22 23:15:34 +0400

    commit

---
+++ level 37/classwork/lesson.py
@@ -0,0 +1,6 @@
+def greet(n1,n2):
+    Sum, Mult = n1+n2,n1*n2
+    return Sum,Mult
+x,y = greet(10,15)
+print(x)
+print(y)
---
+++ level 37/homework/lesson.py
@@ -0,0 +1,11 @@
+def greet(name):
+    return "Hala" + name
+
+print(greet("Madrid"))
+
+def length(word):
+    return len(word)
+print(length("Hala Madrid"))
+
+# return აბრუნებს პასუხს(შედეგს),
+# print ს გამოაქვს ტერმინალში ჩემი შედეგი (პასუხი)
