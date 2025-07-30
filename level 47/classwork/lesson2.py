73f88e526ae7a9d90a3e0e75329a93afd0706194
Kyllian-Mbappe2025 <sandrokvinchia2012@gmail.com>
2025-07-30 21:49:26 +0400

    commit

---
+++ level 47/classwork/lesson2.py
@@ -0,0 +1,12 @@
+name = ["სანდრო","ელენე","გიო","ანდრია"]
+print(name[1:3])
+print(name[-4:-2])
+
+number = [12,9,7,5,10,14]
+print(number[:5])
+
+country = "Poland"
+print(country[::-3])
+
+niga = ["a","b","c","d","e","f","g"]
+print(niga[2:7:2])
